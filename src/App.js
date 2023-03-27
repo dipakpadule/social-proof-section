62ed1c96fea47b740971bce0e81aee8f503e5e37
dipak <sarkar3115@gmail.com>
2023-03-27 19:28:12 +0530

    desktop done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import anne from './images/image-anne.jpg';
+import colton from './images/image-colton.jpg';
+import irene from './images/image-irene.jpg';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='sp-card'>
+      {/* top section */}
+      <section className='sp-card-top'>
+        <div className='sp-card-top-summary'>
+          <h1>10,000+ of our users love our products.</h1>
+          <p>We only provide great products combined with excellent customer service.See what our satisfied customers are saying about our services.</p>
+        </div>
+        <div className='sp-card-top-ratings rate1'>
+          <div className='spc-rating-card'>
+            <div className='star'></div>
+            <h3>Rated 5 Stars in Reviews</h3>
+          </div>
+          <div className='spc-rating-card rate2'>
+            <div className='star'></div>
+            <h3>Rated 5 Stars in RepGuru</h3>
+          </div>
+          <div className='spc-rating-card rate3'>
+            <div className='star'></div>
+            <h3>Rated 5 Stars in BestTech</h3>
+          </div>
+        </div>
+      </section>
+
+      {/* bottom section */}
+      <section className='sp-card-bottom'>
+        <article className='sp-card-reviews rv1'>
+          <div className='sp-card-reviews-details'>
+            <img src={anne} alt='' />
+            <section className='sp-card-user'>
+              <h2>Colton Smith</h2>
+              <span>Verified Buyer</span>
+            </section>
+          </div>
+          <div className='sp-card-review'>
+              "We needed the same printed design as the one we had ordered a week prior.
+                Not only did they find the original order, but we also received it in time.
+                Excellent!"
+          </div>
+        </article>
+        <article className='sp-card-reviews rv2'>
+          <div className='sp-card-reviews-details'>
+            <img src={colton} alt='' />
+            <section className='sp-card-user'>
+              <h2>Irene Roberts</h2>
+              <span>Verified Buyer</span>
+            </section>
+          </div>
+          <div className='sp-card-review'>
+              "Customer service is always excellent and very quick turn around. Completely
+                delighted with the simplicity of the purchase and the speed of delivery."
+          </div>
+        </article>
+        <article className='sp-card-reviews rv3'>
+          <div className='sp-card-reviews-details'>
+            <img src={irene} alt='' />
+            <section className='sp-card-user'>
+              <h2>Anne Wallace</h2>
+              <span>Verified Buyer</span>
+            </section>
+          </div>
+          <div className='sp-card-review'>
+              "Put an order with this company and can only praise them for the very high
+                standard. Will definitely use them again and recommend them to everyone!"
+          </div>
+        </article>
+      </section>
+    </section>
   );
 }
 
---
+++ style-guide.md
@@ -0,0 +1,32 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 23.43rem
+- Desktop: 90rem
+
+## Colors
+
+### Primary
+
+Very-Dark-Magenta: hsl(300, 43%, 22%);
+Soft-Pink: hsl(333, 80%, 67%);
+
+### Neutral
+
+Dark-Grayish-Magenta: hsl(303, 10%, 53%);
+Light-Grayish-Magenta: hsl(300, 24%, 96%);
+White: hsl(0, 0%, 100%);
+
+## Typography
+
+### Body Copy
+
+- Font-size: 0.9rem;
+
+### Font
+
+- Font-Family: 'League Spartan';
+- Weights: 400, 500, 700
